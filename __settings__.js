377fcfa5b83d1026dfa881f932039bcdc74b3669
EugeneO0o <chenyanjin.eugene@gmail.com>
2025-07-14 12:06:14 +0800

    Files

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2280185.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 240435677, 240435663 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/240435707/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/240435704/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/240435706/1/ammo.js', 'preload' : true},
+];
